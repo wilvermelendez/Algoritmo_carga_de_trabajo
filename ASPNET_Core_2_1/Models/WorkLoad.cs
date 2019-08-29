2c903c8dc2170cfe6f1e59cb5ae08630ec76815e
Hector Hernandez <hector.aha@gmail.com>
2019-08-28 22:19:59 -0600

    Auto stash before rebase of "origin/master"

--- ASPNET_Core_2_1/Models/WorkLoad.cs
+++ ASPNET_Core_2_1/Models/WorkLoad.cs
@@ -5,8 +5,9 @@ namespace ASPNET_Core_2_1.Models
 {
     public class WorkLoad
     {
+        public Type Type { get; set; }
         public List<Operator> Operators { get; set; }
 
 
-    }
+   }
 }

7f3a7f5446abef201acee12830fe7a7d91b26332
Wil Melendez <wilver.melendez@gmail.com>
2019-08-29 17:02:11 -0600

    Agregar clases

---
+++ ASPNET_Core_2_1/Services/WorkLoadService.cs
@@ -0,0 +1,26 @@
+﻿using ASPNET_Core_2_1.Models;
+using System.Collections.Generic;
+
+namespace ASPNET_Core_2_1.Services
+{
+    public class WorkLoadService
+    {
+        public IJsonService JsonService { get; set; }
+
+        public WorkLoadService(IJsonService jsonService)
+        {
+            JsonService = jsonService;
+        }
+
+        public List<WorkLoad> GetWorkLoads()
+        {
+
+        }
+
+        public bool SaveWorkLoad()
+        {
+
+
+        }
+    }
+}

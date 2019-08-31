36d4ec8147d531a9f1ad5ff99f27187076d601a6
Wilver Melendez <wilver.melendez@gmail.com>
2019-08-30 21:32:42 -0600

    Agregar servicios para carga de trabaja y parametros

---
+++ ASPNET_Core_2_1/Models/Parameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_Core_2_1.Models
+{
+    public class Parameter
+    {
+        public int Id { get; set; }
+        public int HoursPerJobs { get; set; }
+        public int PricePerJob { get; set; }
+        public int Percentage { get; set; }
+    }
+}

10fa7f8fb98cdc93eaeba8fb6ad40eba5112c522
Wilver Melendez <wilver.melendez@gmail.com>
2019-08-30 20:39:11 -0600

    Agregar Guardar json

--- ASPNET_Core_2_1/Services/JsonService.cs
+++ ASPNET_Core_2_1/Services/JsonService.cs
@@ -29,5 +29,22 @@ namespace ASPNET_Core_2_1.Services
 
         }
 
+        public bool SaveJson<T>(List<T> data, string path) where T : class
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(data.ToArray());
+                //write string to file
+                File.WriteAllText(path, string.Empty);
+                File.WriteAllText(path, json);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
     }
 }
